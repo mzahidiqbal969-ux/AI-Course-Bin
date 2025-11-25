c48f71406401b77a30e5aacc58e94b82f87ee6a1
mzahidiqbal969-ux <m.zahidiqbal969@gmail.com>
2025-11-25 20:58:35 +0500

    My code

--- Hello.py
+++ Hello.py
@@ -1,1 +1,9 @@
-print("Hello World")
+print("Hello World") #my first code
+book_id=556
+print(book_id)
+print(type(book_id))
+book_price=45.5
+print(book_price)
+print(type(book_price))
+book_title="zero to one"
+print(book_title)
