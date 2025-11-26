bdc84c7f56ff454fc04e40c168f3960542a302f2
mzahidiqbal969-ux <m.zahidiqbal969@gmail.com>
2025-11-27 02:00:15 +0500

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -7,3 +7,57 @@ print(book_price)
 print(type(book_price))
 book_title="zero to one"
 print(book_title)
+student=["Hassan",2017,94.4,"Muzaffargarh"]
+print(student)
+print(student[0])
+marks=[65,67,76,89,90,97]
+print(marks[1:4])
+str="zahidiqbal"  #string slicing
+print(str[0:5])
+#length of string
+str="zahidiqbal"
+print(len(str))
+#concatenation
+firstname="zahid"
+lastname="iqbal"
+fullname=firstname+" " +lastname
+print(fullname)
+#LIST SLICING
+marks=[67,65,78,87,98,93]
+print(marks[1:4])
+print(marks[:4]) #it will start from zero
+print(marks[1:])#it will start from index 1 to end
+print(marks[-3:-1])
+#list
+list=[2,1,3]
+list.append(5)
+print(list)
+list.sort()
+print(list)
+list_1=["d","e","a","b","f"]
+list_1.sort()
+print(list_1)
+#tupple in python
+tup=(1,3,5,7,3,)
+print(tup)
+print(type(tup))
+print(tup[0])
+print(tup[1])
+#slicing in tuple
+print(tup[1:3])
+#index in tuple
+print(tup.count(3))
+#write a program to generate a list of three movies
+list_2=["anarkali","albela","bazigar"]
+print(list_2)
+# movies=[]
+# mov1=input("enter the name of 1st movie :")
+# mov2=input("enter the name of 2nd movie :")
+# mov3=input("enter the name of 3rd movie :")
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov2)
+# print(movies)
+ count= 1
+ while count<=5 :
+     print("hello world")
