d629fbd59e74004896f09b75dc42406d59a5d571
mzahidiqbal969-ux <m.zahidiqbal969@gmail.com>
2025-12-01 20:24:41 +0500

    My code practice

---
+++ practic2ndweek.py
@@ -0,0 +1,18 @@
+City_Name="Lahore"
+City_Long=46.6
+Is_Cpital=False
+City_Zip_Code=7400
+print("city name:",City_Name)
+print(City_Long)
+print(Is_Cpital)
+print(City_Zip_Code)
+print(type(City_Name))
+print(type(City_Long))
+print(type(Is_Cpital))
+print(type(City_Zip_Code))
+name=input("enter your name : ")
+print("hello world: " , name)
+age=int(input(" enter age: "))
+print("enter your age :", age)
+cityLong=float(input("enter your city longitude"))
+print("enter your city longitude:",cityLong)
