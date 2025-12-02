c1821e1bfbbe2138262bdb5978f5029ad880d736
mzahidiqbal969-ux <m.zahidiqbal969@gmail.com>
2025-12-02 19:53:00 +0500

    solving my issue

---
+++ practice_day2_2ndweek.py
@@ -0,0 +1,11 @@
+student_age= 43
+print(student_age)
+print(student_age+5)
+updated_age=student_age+5
+print(updated_age)
+print(student_age-3*4)
+print((student_age-3)*4)
+student_height=5.9
+print(student_height/2)
+updated_student_height=student_height/2
+print(updated_student_height)
