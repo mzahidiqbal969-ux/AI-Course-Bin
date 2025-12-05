a16fd7aeb8a4731f25557ae538675713ad318c3f
mzahidiqbal969-ux <m.zahidiqbal969@gmail.com>
2025-12-05 23:09:19 +0500

    check my status

---
+++ homework_2ndweek.py
@@ -0,0 +1,79 @@
+#Write a Python program that asks the user for their name and city, and then prints a sentence like:
+#“My name is Ali and I live in Lahore.”
+name="Ali"
+city="Lahore"
+print("My name is", name ,"and I live in", city)
+# Ask the user to enter any word.
+# Print the following:
+# First 2 letters
+# Last 2 letters
+# Middle letter (only one)
+# word= input("enter my word:")
+# print("first 2 letters:", word[0:2])
+# print("last 2 letters:", word[-2:-1])
+
+# sentence=input("i love my pakistan")
+# count=len(sentence.replace(" ",""))
+# print("print the sentence without gap:",count)
+#Create variables a, b, and c with values 5, 10, and 15. Print their sum.
+a=5
+b=10
+c=15
+print(a+b+c)
+#Store your name, age, and city in three variables and print them in one sentence.
+name="zahid"
+age=38
+city="Mahra"
+print("my name is",name,"and my age is",age,"and i live in",city)
+#Assign two numbers to variables and swap their values
+a=5
+b=10
+print("before swapping")
+print("a:",a)
+print("b:",b)
+a,b=b,a
+print("\n after swapping")
+print("a:",a)
+print("b:",b)
+#Create a variable radius and calculate the area of a circle. (πr²)
+rad=5
+pi=3.1414
+area=pi*rad*rad
+print(area)
+#Store the price and quantity of an item in variables and print the total bill.
+shoesprice=200
+quantity=10
+bill=shoesprice*quantity
+print(bill)
+#Create two variables: length and width. Find the perimeter of a rectangle.
+len=10
+wid=3
+perimeter=2*(len+wid)
+print(perimeter)
+#Store an integer, float, and string in three variables and print their types
+number=10
+height=5.6
+name="hassan"
+print(type(number))
+print(type(height))
+print(type(name))
+#Take your friend's name in a variable and print a welcome message.
+name="hassan"
+print("welcome",name)
+#Create variables math, physics, chemistry and print the total marks.
+math=70
+physics=90
+chemistry=80
+print("total marks:",math+physics+chemistry)
+#Assign two variables x=20 and y=6. Print the result of x+y, x−y, x×y, x÷y
+x=20
+y=6
+value1=x+y
+print(value1)
+value2=x-y
+print(value2)
+value3=x*y
+print(value3)
+value4=x/y
+print(value4)
+
