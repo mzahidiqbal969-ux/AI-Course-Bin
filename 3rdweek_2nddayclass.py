aef166b27d7834a795c3aafadbfab6817e9cb197
mzahidiqbal969-ux <m.zahidiqbal969@gmail.com>
2025-12-19 00:40:11 +0500

    my work

--- 3rdweek_2nddayclass.py
+++ 3rdweek_2nddayclass.py
@@ -1,7 +1,108 @@
-age=4
-print(age)
-print(type(age))
-height=5.3
-print(height)
-print(type(height))
+#Check if a number is multiple of 5
+# num=int(input("enter the number:"))
+# if num %5==0:
+#     print("it is multiple of 5")
+# else:
+#     print("it is not multiple of 5")
+#Input password. If password = "admin123" print "Correct" else "Incorrect"
+# password=input("enter password: ")
+# if password=="admin123":
+#     print("correct")
+# else:
+#     print("Incorrect")
+# #Input a string. If it starts with 'A' print "Starts with A" else "Does not start with A"
+# str=input("enter the character:")
+# if str=="A":
+#     print("Starts with A")
+# else:
+#     print("Does not start with A")
+#If a number is divisible by both 2 and 3, print "Divisible", else "Not Divisible".
+# num=int(input("enter the number:"))
+# if num % 2 ==0 and num % 3 ==0:
+#     print("Divisible")
+# else:
+#     print("Not Divisible")
+#Check if a number is between 1 and 100 else print "Out of Range"
+# num=int(input("enter the number:"))
+# if num>=1 and num<=100:
+#     print("print the number:",num)
+# else:
+#     print("Out of Range")
+#Check if a person has enough balance (balance ≥ amount)
+
+# balance = int(input("Enter balance: "))
+# amount = int(input("Enter amount to withdraw: "))
+
+# if balance >= amount:
+#     print("Enough balance")
+# else:
+#     print("Not enough balance")
+# #Check if last digit of a number is 0
+# num = int(input("Enter a number: "))
+
+# if num % 10 == 0:
+#     print("The last digit is 0")
+# else:
+#     print("The last digit is NOT 0")
+#Input length and width. If both are equal, print "Square" else "Rectangle"
+# l = int(input("Enter value of length: "))
+# w = int(input("Enter value of width: "))
+# if l==w:
+#     print("Square")
+# else:
+#     print("Rectangle")
+# ch = input("Enter a character: ")
+
+# if ch.isdigit():
+#     print("Digit")
+# else:
+#     print("Not a digit")
+# marks = int(input("Enter marks: "))
+# if marks>=90:
+#     print("Grade A")
+# elif marks>=80:
+#     print("Grade B")
+# elif marks>=70:
+#     print("Grade C")
+# elif marks>=60:
+#     print("Grade D")
+# else:
+#     print("Grade F")    
+# num = int(input("Enter the number: "))
+# if num==1:
+#     print("January")
+# elif num==2:
+#     print("February")
+# elif num==3:
+#     print("March")
+# elif num==4:
+#     print("April")
+# elif num==5:
+#     print("May")
+# elif num==6:
+#     print("June")
+# elif num==7:
+#     print("July")
+# elif num==8:
+#     print("August")
+# elif num==9:
+#     print("September")
+# elif num==10:
+#     print("October")
+# elif num==11:
+#     print("November")
+# elif num==12:
+#     print("December")
+# else:
+#     print("Give the number in range of 1-12")
+#Check if a number is divisible by 2:
+#If yes, check if divisible by 4
+# num = int(input("Enter the number: "))
+# if num%2==0:
+#     print("number is divisible by 2")
+#     if num%4==0:
+#         print("number is divisible by 4")
+# else:
+#     print("other number")
+
 
