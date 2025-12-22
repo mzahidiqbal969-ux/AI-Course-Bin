5296c0602f86c5f2556650daab87aed860e27c15
mzahidiqbal969-ux <m.zahidiqbal969@gmail.com>
2025-12-22 20:36:47 +0500

    Create 22dececmber.py
    my work

---
+++ 22dececmber.py
@@ -0,0 +1,16 @@
+newspaper_id=45
+print(newspaper_id)
+print(type(newspaper_id))
+newspapertitle="dawn"
+print(newspapertitle)
+print(type(newspapertitle))
+newspaperprice=34.5
+print(newspaperprice)
+print(type(newspaperprice))
+editor=input("enter editor name:")
+print("hello editor: "+editor)
+#print("upper:",+ editor.upper())
+newseditorname="zahid"
+for i in "zahid":
+    print(i)
+print(len(newseditorname))
